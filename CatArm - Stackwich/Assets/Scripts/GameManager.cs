cae0083d87de676efe3f75e3741490fbef2df290
cartholomew <36948010+cartholomew@users.noreply.github.com>
2019-10-07 13:21:53 -0400

    Started basic stacking functions.
    Starting block with moving cubes.
    Working Spawner.
    (There are a ferw issues that will be fixed).

---
+++ CatArm - Stackwich/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (MovingCube.CurrentCube != null)
+                MovingCube.CurrentCube.Stop();
+
+            FindObjectOfType<CubeSpawner>().SpawnCube();
+        }
+    }
+}
---
+++ CatArm - Stackwich/Assets/Scripts/MovingCube.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MovingCube : MonoBehaviour
+{
+    public static MovingCube CurrentCube { get; private set; }
+    public static MovingCube LastCube { get; private set; }
+
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    private void OnEnable()
+    {
+        if (LastCube == null)
+            LastCube = GameObject.Find("Start").GetComponent<MovingCube>();
+
+        CurrentCube = this;
+        GetComponent<Renderer>().material.color = GetRandomColor();
+
+        transform.localScale = new Vector3(LastCube.transform.localScale.x, transform.localScale.x, LastCube.transform.localScale.z);
+    }
+
+    private Color GetRandomColor()
+    {
+        return new Color(UnityEngine.Random.Range(0, 1f), UnityEngine.Random.Range(0, 1f), UnityEngine.Random.Range(0, 1f));
+    }
+
+    internal void Stop()
+    {
+        moveSpeed = 0;
+        float hangover = transform.position.z - LastCube.transform.position.z;
+
+        if (Mathf.Abs(hangover) >= LastCube.transform.localScale.z)
+        {
+            LastCube = null;
+            CurrentCube = null;
+            SceneManager.LoadScene(0);
+        }
+
+        float direction = hangover > 0 ? 1f : -1f;
+
+        LastCube = this;
+    }
+
+    private void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * moveSpeed;
+    }
+}
